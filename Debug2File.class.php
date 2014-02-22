ba3edf5a60897b701f73614d0094f6519302d94e
mhendershott <mhendershott@gmail.com>
2014-02-21 23:50:34 -0500

    Create Debug2File.class.php

---
+++ Debug2File.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Debug/Logging/Error Handling
+*/
+class Debug2File
+{
+	private $loggingLevel = 7;
+	
+	function write ($code=7, $detail="Unknown") {
+
+		/* Code levels
+		0 - Emergency (emerg)
+		1 - Alerts (alert)
+		2 - Critical (crit)
+		3 - Errors (err)
+		4 - Warnings (warn)
+		5 - Notification (notice)
+		6 - Information (info)
+		7 - Debug (debug)
+		*/
+		
+		$loggingLevel = 7; // Set to highest level of debugging you wish to use.
+
+		if ($code <= $this->loggingLevel) {
+			$log = strtotime("now").";$code;$detail;$user;\n";
+			$fp = fopen('debuglogs.txt', 'a');
+			fwrite($fp, $log);
+			fclose($fp);
+		}
+
+		// the content of 'data.txt' is now 123 and not 23!
+
+	}
+
+}
+?>
